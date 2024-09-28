3f82d799d5b18031297cf0be3257879813688bad
Can Vardar <ismailcanvardar@gmail.com>
2024-09-28 19:51:16 +0300

    refactor: update `MonitorController` class

--- src/monitor_controller.py
+++ src/monitor_controller.py
@@ -6,7 +6,8 @@ import re
 import subprocess
 import sys
 
-BACKUP_FILE_PATH = "/var/tmp/sc_data.bin"
+BACKUP_FILE_PATH = "/var/tmp/i3-screen-utl.data.bin"
+
 # reference: https://www.thinkwiki.org/wiki/Xorg_RandR_1.2#Output_port_names
 XRANDR_DISPLAY_TYPES = [
     "VGA",
@@ -26,45 +27,6 @@ XRANDR_DISPLAY_TYPES = [
     "DVI",
     "DP",
 ]
-
-
-class Arg:
-    def __init__(self, monitor_num, locate_to=None, side_monitor_num=None):
-        self.monitor_num = monitor_num
-        self.locate_to = locate_to
-        self.side_monitor_num = side_monitor_num
-
-    @staticmethod
-    def get_arg(index):
-        try:
-            return sys.argv[index]
-        except IndexError:
-            return None
-
-    @classmethod
-    def build(cls):
-        try:
-            mn = cls.get_arg(1)
-            l_to = cls.get_arg(2)
-            smn = cls.get_arg(3)
-
-            if mn is not None:
-                mn = int(mn) - 1
-                if mn < 0:
-                    raise Exception("Wrong 'monitor_num' value.")
-
-            if l_to is not None:
-                if str(l_to) != "left" and str(l_to) != "right":
-                    raise Exception("'location_to' value must be 'left' or 'right'.")
-
-            if smn is not None:
-                smn = int(smn) - 1
-                if smn < 0:
-                    raise Exception("Wrong 'location_of' value.")
-
-            return cls(mn, l_to, smn)
-        except Exception:
-            sys.exit()
 
 
 class Backup:
@@ -115,7 +77,7 @@ class MonitorController:
         Backup.save_backup_data(backup_data)
 
     @staticmethod
-    def toggle_monitor(cls):
+    def toggle_monitor(cls, monitor_number, locate_to, locate_of):
         if os.path.exists(BACKUP_FILE_PATH) is False:
             with open(BACKUP_FILE_PATH, "w") as backup_file:
                 backup_file.close()
@@ -135,12 +97,10 @@ class MonitorController:
         ]
 
         try:
-            args = Arg.build()
-
-            monitor_name = options[args.monitor_num]
+            monitor_name = options[monitor_number]
             side_monitor_name = None
-            if args.side_monitor_num is not None:
-                side_monitor_name = options[args.side_monitor_num]
+            if locate_of is not None:
+                side_monitor_name = options[locate_of]
 
             backup_data = Backup.get_backup_data(options)
 
@@ -149,19 +109,15 @@ class MonitorController:
             if is_turned_on is True:
                 cls.turn_off_monitor(monitor_name, backup_data)
             else:
-                if args.locate_to is None or side_monitor_name is None:
+                if locate_to is None or side_monitor_name is None:
                     raise Exception(
                         "'locate_to' or 'location_of' arguments cannot be None."
                     )
 
                 cls.turn_on_monitor(
-                    monitor_name, args.locate_to, side_monitor_name, backup_data
+                    monitor_name, locate_to, side_monitor_name, backup_data
                 )
         except IndexError:
             raise Exception("Invalid monitor number.")
         except Exception:
             sys.exit()
-
-    @staticmethod
-    def test():
-        print("a")
