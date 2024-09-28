1f2dfe442784fe8a8b5686511f1e1d89d1daaf17
Can Vardar <ismailcanvardar@gmail.com>
2024-09-28 19:49:49 +0300

    feat: add control-lockscreen script
    `prevent-lock` script is renamed to `control-lockscreen` and also automatic process killing is removed.

--- src/bin/control-lockscreen.sh
+++ src/bin/control-lockscreen.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-ps -aux | grep prevent-lock | grep -v grep | awk '{ print $2 }' | xargs kill -9
-
 while true; do
   if playerctl metadata --format '{{mpris:artUrl}}' | grep -E -q "\.(mp4|mkv|avi|webm|mov|flv|wmv|mpg|mpeg)$"; then
     xautolock -disable
