42e8e84d10a94d4352b8fa40fc433172fd606f1f
Can Vardar <ismailcanvardar@gmail.com>
2024-09-28 19:52:44 +0300

    refactor(main): implement `Args` class and app logic

--- main.py
+++ main.py
@@ -6,12 +6,27 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from workspace_formatter import WorkspaceFormatter
+from lockscreen import Lockscreen
 from monitor_controller import MonitorController
+
+from args import Args
 
 
 def main():
-    WorkspaceFormatter.try_me()
-    MonitorController.test()
+    args = Args()
+
+    match args.method:
+        case "organize":
+            if args.action == "load":
+                WorkspaceFormatter.load_workspaces(args.workspaces)
+            elif args.action == "save":
+                WorkspaceFormatter.save_workspaces(args.workspaces)
+        case "lockscreen":
+            Lockscreen.control_lockscreen()
+        case "toggle":
+            MonitorController.toggle(
+                args.monitor_number, args.locate_to, args.locate_of
+            )
 
 
 if __name__ == "__main__":
